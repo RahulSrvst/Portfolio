ac63de6708d09275bd5a4ed45c55ce41918e9bf3
Rahul Srivastava <rahulsrivast356@gmail.com>
2025-02-03 18:23:09 +0530

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,181 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Rahul Sriastava",
+        description: "I am a software engineer with expertise in full-stack & MERN web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full-Stack Developer || MERN Stack Developer",
+        photo: "../Photo.png",
+        email: "rahulsrivastava1332003@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://x.com/i/flow/login?redirect_after_login=%2FRahulSr54520850",
+		github: "https://github.com/RahulSrvst",
+		linkedin: "https://www.linkedin.com/in/rahulsrvst07",
+		instagram: "https://www.instagram.com/rahul.srvst/",
+		leetcode: "https://leetcode.com/u/RahulSrvst7/",
+    },
+
+    // ============ PROJECTS ============
+projects: [
+    {
+        "title": "Vue Ecomm",
+        "description": "An e-commerce platform built with Vue, Laravel, and Tailwind, providing a seamless shopping experience.",
+        "technologies": "PHP, Laravel, Vue, Tailwind",
+        "github": "https://github.com/RahulSrvst/vue_ecom",
+        "link": "https://github.com/RahulSrvst/vue_ecom"
+    },
+
+    {
+        "title": "Smart Campus",
+        "description": "A full-stack web application developed using Nuxt3, Express, and PostgreSQL for campus management during my internship.",
+        "technologies": "Nuxt, Tailwind, Express, PostgreSQL",
+        "github": "https://github.com/RahulSrvst/SmartCampus",
+        "link": "https://github.com/RahulSrvst/SmartCampus"
+    },
+
+    {
+        "title": "Gym Fluencer",
+        "description": "A fitness influencer platform where users can track workouts, share progress, and connect with like-minded individuals.",
+        "technologies": "React, Tailwind, Tensorflow",
+        "github": "https://github.com/RahulSrvst/Gym-Fluencer",
+        "link": "https://gym-fluencer-two.vercel.app/"
+    },
+
+    {
+        "title": "Chat App",
+        "description": "A real-time chat application that allows users to create chat rooms and communicate seamlessly using Firebase.",
+        "technologies": "React, Tailwind, Firebase",
+        "github": "https://github.com/RahulSrvst/Chat-App",
+        "link": "https://thechatapp.netlify.app/"
+    },
+
+    {
+        "title": "Password Generator",
+        "description": "A secure password generator that creates strong and customizable passwords for better security.",
+        "technologies": "React, Tailwind, TypeScript",
+        "github": "https://github.com/RahulSrvst/Password-Generator",
+        "link": "https://password-generator.netlify.app/"
+    },
+
+    {
+        "title": "Airline Reservation System",
+        "description": "A C++-based airline reservation system implementing OOP principles for efficient flight booking and management.",
+        "technologies": "C++, OOP",
+        "github": "https://github.com/RahulSrvst/Airline-Reservation-System-Project",
+        "link": "https://github.com/RahulSrvst/Airline-Reservation-System-Project"
+    }
+],
+
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Shri Ramswaroop Memorial College Of Engineering & Management",
+            degree: "Bachelor of Technology in Computer Science",
+            duration: "2020 - 2024",
+            image: "spvttc.png"
+        },
+        {
+            school: "Sumitra Public Inter College",
+            degree: "Assosiate Degree",
+            duration: "2016 - 2020",
+            image: "spic.webp"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "React Developer Intern",
+            company: "Aara Groups Pvt Ltd, Inc.",
+            duration: "Aug 2024 - Jan 2024 (5.5 Months)",
+            image: "aara.webp",
+            descriptions: [
+                "Completed an extensive training program in full-stack web development.",
+                "Actively contributed to the ongoing development of a web application.",
+                "Mentored by senior developers on best practices and code optimization.",
+            ]
+        },
+        {
+            position: " Web Developer",
+            company: "TCR INNOVATION ",
+            duration: "April 2023 - July 2023 (3 Month)",
+            image: "tcr.webp",
+            descriptions: [
+                "Developed Python scripts to automate document score calculations.",
+                "Developed a company website as part of an ICT-related work immersion program."
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Mastering Data Structures & Algorithms using C and C++",
+            description: "Provided by Udemy & Authorized by Udemy",
+            icon: "udemy",
+            link: "https://www.udemy.com/certificate/UC-968a725a-f262-453d-ad46-4d9c075217da/",
+        },
+        {
+            title: "Data Science 101",
+            description: "Provided by Coursera & Authorized by IBM",
+            icon: "ibm",
+            link: "https://courses.cognitiveclass.ai/certificates/ec84827d8df249618d80b7f2958e99f0",
+        },
+        {
+            title: "TCR MERN FULL STACK DEVELOPMENT INTERNSHIP PROGRAM",
+            description: "Provided by TCR & Authorized by TCR",
+            icon: "TCR Innovation",
+            link: "https://verification.givemycertificate.com/v/965230a7-a49a-4240-a609-48eab506eeb2"
+        },
+        // {
+        //     title: "Technical Support Fundamentals Certificate",
+        //     description: "Provided by Coursera & Authorized by Google",
+        //     icon: "google",
+        //     link: "https://www.coursera.org/account/accomplishments/verify/P9EH5HNYRESZ"
+        // },
+        // {
+        //     title: "Introduction to Web Development with HTML, CSS, JavaScript Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/7b88eb00-e1f9-4ae6-be2a-3b2ae8983c44"
+        // },
+        // {
+        //     title: "Introduction to Cloud Computing Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/514b694a-8dc4-418d-9af4-61908dc29ca8"
+        // },
+        // {
+        //     title: "Django Application Development with SQL and Databases Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/a36dd8a3-1050-4c4e-bcef-97b39babf55e"
+        // },
+        // {
+        //     title: "Front-End Developer (React) Certificate",
+        //     description: "Provided by HackerRank",
+        //     icon: "hackerrank",
+        //     link: "https://www.hackerrank.com/certificates/631ac3acf267"
+        // },
+        // {
+        //     title: "Software Engineer Certificate",
+        //     description: "Provided by HackerRank",
+        //     icon: "hackerrank",
+        //     link: "https://www.hackerrank.com/certificates/efdbdd44eb7d"
+        // }
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at rahulsrivastava1332003@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Rahul Srivastava. All Rights Reserved"
+}
+
+export default info
